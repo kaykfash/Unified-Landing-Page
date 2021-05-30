dd2df6d9325feea14ad4a27da46151bd217ecdfb
Kehinde Fashogon <kkoladefash@yahoo.com>
2021-05-30 08:51:37 +0100

    Unified (SkillUp Internship) Landing page        Stylesheet         Index page         JavaScript

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+let count_down = new Date('06/17/2021 00:00:00').getTime();
+let x = setInterval(() => countDown(), second);
+
+function countDown() {
+  let now = new Date(Date.now()).getTime();
+  let diff = count_down - now;
+
+  document.getElementById('days').innerText = Math.floor(diff / day);
+  document.getElementById('hours').innerText = Math.floor(diff % day / hour);
+  document.getElementById('minutes').innerText = Math.floor(diff % hour / minute);
+  document.getElementById('seconds').innerText = Math.floor(diff % minute / second);
+}
+
+function resetCountdown() {
+  clearInterval(x);
+  let date_end = document.form_main.date_end.value;
+  count_down = new Date(`${date_end} 00:00:00`).getTime();
+  x = setInterval(() => countDown(), second);
+}
+
+
+
+document.getElementById("close").onclick = function(){
+    if(close.style,display === "block"){
+        close.style.display = "none";
+    } else{
+        close.style.display = "block" 
+    }
+}
+
